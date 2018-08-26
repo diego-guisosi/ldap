c15b30bd3d0a78abb0033129e9188d029a0193ef
Diego <diego.guisosi@gmail.com>
2018-08-26 12:35:58 -0300

    turn ldap-search into a web project

--- ldap-search/src/main/java/br/com/dixy/ldap/LdapSearchApplication.java
+++ ldap-search/src/main/java/br/com/dixy/ldap/LdapSearchApplication.java
@@ -1,12 +1,30 @@
 package br.com.dixy.ldap;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-@SpringBootApplication
-public class LdapSearchApplication {
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "br.com.dixy.ldap")
+@PropertySource("classpath:application.properties")
+public class LdapSearchApplication extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LdapSearchApplication.class, args);
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { LdapSearchApplication.class };
 	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[0];
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/*" };
+	}
+
 }
